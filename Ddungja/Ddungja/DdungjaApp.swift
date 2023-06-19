1d48021d0bae1ef4bc08ca64816a0c519f693b4d
ohsg0226 <ohsg0226@naver.com>
2023-06-19 16:15:51 +0900

    ✨ [feat] : Xcode 프로젝트 생성

---
+++ Ddungja/Ddungja/DdungjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DdungjaApp.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct DdungjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
