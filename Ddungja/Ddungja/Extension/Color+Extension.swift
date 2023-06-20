21027551cef8fa307eefdd2e293acfb439391d67
ohsg0226 <ohsg0226@naver.com>
2023-06-20 15:29:08 +0900

    💄 [style] : 카카오 로그인 버튼 칼라 추가

---
+++ Ddungja/Ddungja/Extension/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/06/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let kakaoContainerColor = Color("KakaoContainerColor")
+    let kakaoLabelColor = Color("KakaoLabelColor")
+    let kakaoSymbolColor = Color("KakaoSymbolColor")
+}
