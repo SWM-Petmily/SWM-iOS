3ee38f97ccd28d68d61680f29ba34158be254c03
ohsg0226 <ohsg0226@naver.com>
2023-07-02 21:30:29 +0900

    [style] : Presentation 및 UserProfile 폴더 구성

---
+++ Ddungja/Ddungja/Presentations/UserProfiles/Views/UserProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfileView.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/07/02.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfileView()
+    }
+}
