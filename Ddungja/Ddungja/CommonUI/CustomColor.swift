d22157ea3064ad740a14a0f5e51f5f6b4d15edb9
ohsg0226 <ohsg0226@naver.com>
2023-07-04 22:20:59 +0900

    [chore] : Custom Color 주석제거(#3)

--- Ddungja/Ddungja/CommonUI/CustomColor.swift
+++ Ddungja/Ddungja/CommonUI/CustomColor.swift
@@ -25,25 +25,7 @@ enum CustomColor: String {
     case disabledTextColor
     case buttonBackgroundColor
     case activeTextColor
-    
-//    var customValue: Color {
-//        switch self {
-//        case .mainTextColor:
-//            return Color("MainTextColor")
-//        case .mainColor:
-//            return Color("MainColor")
-//        case .mainBackgroundColor:
-//            return Color("MainBackgroundColor")
-//        case .subColor:
-//            return Color("SubColor")
-//        case .disabledTextColor:
-//            return Color("DisabledTextColor")
-//        case .buttonBackgroundColor:
-//            return Color("ButtonBackgroundColor")
-//        case .activeTextColor:
-//            return Color("ActiveTextColor")
-//        }
-//    }
+
     var customValue: String {
         switch self {
         case .mainTextColor:
