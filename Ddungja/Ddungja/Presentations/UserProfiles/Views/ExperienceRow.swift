9f70020dd43186f620b43109abc726d6238872e2
ohsg0226 <ohsg0226@naver.com>
2023-07-08 20:18:40 +0900

    [feat] : 반려동물 종, 양육기간 추가하는 Row View 생성

--- Ddungja/Ddungja/Presentations/UserProfiles/Views/ExperienceRow.swift
+++ Ddungja/Ddungja/Presentations/UserProfiles/Views/ExperienceRow.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct ExperienceRow: View {
+    
+    @State var dog = ""
+    @State var year = ""
+    @State var month = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 10) {
+            Text("반려동물 종")
+                .applySubtitle(color: .mainTextColor)
+            
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.mainBackground)
+                        .cornerRadius(10)
+                        .frame(height: 54)
+                        .frame(maxWidth: .infinity)
+                    
+                    HStack {
+                        TextField("", text: $dog)
+                            
+                        Spacer()
+                        
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .padding()
+                }
+            }
+            
+            Text("양육기간")
+                .applySubtitle(color: .mainTextColor)
+            
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.mainBackground)
+                        .cornerRadius(10)
+                        .frame(height: 54)
+                        .frame(maxWidth: .infinity)
+                    
+                    HStack {
+                        TextField("", text: $year)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.numberPad)
+                            
+                        Spacer()
+                        
+                        Text("년")
+                    }
+                    .padding()
+                }
+                
+                ZStack {
+                    Rectangle()
+                        .fill(Color.mainBackground)
+                        .cornerRadius(10)
+                        .frame(height: 54)
+                        .frame(maxWidth: .infinity)
+                    
+                    HStack {
+                        TextField("", text: $month)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.numberPad)
+                            
+                        Spacer()
+                        
+                        Text("개월")
+                    }
+                    .padding()
+                }
+            }
+        }
     }
 }
 
