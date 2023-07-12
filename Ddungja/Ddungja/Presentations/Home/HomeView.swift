f17b3a9a7e216a9a382211aa8f14a0b0d6a793d7
ohsg0226 <ohsg0226@naver.com>
2023-07-12 16:26:39 +0900

    [feat] : 탭 뷰에 들어갈 초기 View추가

---
+++ Ddungja/Ddungja/Presentations/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Color.red
+    }
+}
+
+struct homeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Ddungja/Ddungja/Presentations/Interest/InterestView.swift
@@ -0,0 +1,20 @@
+//
+//  InterestView.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct InterestView: View {
+    var body: some View {
+        Color.yellow
+    }
+}
+
+struct interestView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterestView()
+    }
+}
