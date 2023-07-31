5879aa81f92a7954e67d35c8a5e9b6d6ebabfee3
ohsg0226 <ohsg0226@naver.com>
2023-07-31 15:00:51 +0900

    [feat] : Bundle Extension추가

---
+++ Ddungja/Ddungja/App/Extension/Bundle+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Bundle+Extension.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/07/25.
+//
+
+import Foundation
+
+extension Bundle {
+    var apiKey: String {
+        guard let file = path(forResource: "Info", ofType: "plist") else { return "" }
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        guard let key = resource["KAKAO_API_LOGIN_KEY"] as? String else {
+            fatalError("API_Key 설정이 안되어 있음.")
+        }
+        return key
+    }
+}
