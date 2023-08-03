ed99285a2181b88dbf32b4024e7f572d997e8a00
ohsg0226 <ohsg0226@naver.com>
2023-08-03 15:56:23 +0900

    [feat] : 상세 게시물 페이지 UI 구현

--- Ddungja/Ddungja/Presentations/Home/ViewModels/DetailPostViewModel.swift
+++ Ddungja/Ddungja/Presentations/Home/ViewModels/DetailPostViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 final class DetailPostViewModel: ObservableObject {
     private let coordinator: CoordinatorProtocol
@@ -15,6 +16,7 @@ final class DetailPostViewModel: ObservableObject {
     
     @Published var detail: DetailPostVO?
     @Published var imagesURLString = [(id: String, url: String)]()
+    @Published var diseaseInfo = [String]()
     
     init(coordinator: CoordinatorProtocol, homeUsecase: HomeUsecaseInterface) {
         self.coordinator = coordinator
@@ -26,9 +28,33 @@ final class DetailPostViewModel: ObservableObject {
             .sink { error in
                 print("getDetailPost error \(error)")
             } receiveValue: { [weak self] vo in
+                print(vo)
                 self?.detail = vo
                 self?.imagesURLString = vo.images.map { (id: UUID().uuidString, url: $0.url) }
+                self?.diseaseInfo = vo.diseases.map { $0 }
             }
             .store(in: &cancellables)
+    }
+    
+    func changIdToProfile(_ id: Int) -> String {
+        switch id {
+        case 1: return "bulldog"
+        case 2: return "dog1"
+        case 3: return "dog2"
+        case 4: return "poodle"
+        default: return "bulldog"
+        }
+    }
+    
+    func changeMonthToAge(_ month: Int) -> String {
+        return month <= 12 ? "\(month)개월" : "\(month/12)년\(month%12)개월"
+    }
+    
+    func changeGenderToImage(_ type: String) -> Image {
+        return type == "MALE" ? Image("Male") : Image("Female")
+    }
+    
+    func pop() {
+        coordinator.pop()
     }
 }
--- Ddungja/Ddungja/Presentations/Home/Views/DetailPostScene.swift
+++ Ddungja/Ddungja/Presentations/Home/Views/DetailPostScene.swift
@@ -34,18 +34,288 @@ struct DetailPostScene: View {
     
     var body: some View {
         GeometryReader { geo in
-            TabView {
-                ForEach(viewModel.imagesURLString, id: \.id) { image in
-                    PetImageView(imageUrl: image.url)
-                        .aspectRatio(contentMode: .fill)
-                        .clipped()
+            VStack {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        TabView {
+                            ForEach(viewModel.imagesURLString, id: \.id) { image in
+                                PetImageView(imageUrl: image.url)
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipped()
+                            }
+                        }
+                        .frame(width: geo.size.width, height: 350)
+                        .tabViewStyle(.page)
+                        
+                        userTitle
+                        
+                        petInfo
+                        petPrice
+                        
+                        healthInfo
+                        diseaseInfo
+                        
+                        reason
+                        
+                        
+                        advantage
+                        
+                        disadvantage
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+                HStack {
+                    VStack(alignment: .center) {
+                        Image(systemName: "heart")
+                            .foregroundColor(.disabledText)
+                            .frame(width: 24, height: 24)
+                        Text("\(viewModel.detail?.likeCount ?? -1)")
+                            .bold()
+                            .applySubtitle(color: .mainTextColor)
+                    }
+                    .padding(.leading, 20)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("입양 신청하기")
+                            .bold()
+                            .applyInner(color: .white)
+                            .frame(height: 52)
+                            .frame(maxWidth: .infinity)
+                    }
+                    .background(Color.main)
+                    .cornerRadius(14)
+                    .padding()
                 }
             }
-            .frame(width: geo.size.width, height: 350)
-            .tabViewStyle(.page)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "chevron.backward")
+                        .onTapGesture {
+                            viewModel.pop()
+                        }
+                }
+            }
         }
         .onAppear {
             viewModel.getDetailPost(1)
         }
+    }
+}
+
+extension DetailPostScene {
+    private var userTitle: some View {
+        HStack(spacing: 14) {
+            ZStack {
+                Circle()
+                    .fill(.gray)
+                    .frame(width: 50, height: 50)
+                Image(viewModel.changIdToProfile(viewModel.detail?.writerProfileImage ?? -1))
+                    .resizable()
+                    .frame(width: 40, height: 40)
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(viewModel.detail?.writer ?? "")
+                    .applyTitle(color: .mainTextColor)
+                    .bold()
+                
+                Text("대한민국, \(viewModel.detail?.region ?? "")")
+                    .applySubtitle(color: .disabledTextColor)
+            }
+        }
+        .padding([.leading, .trailing])
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var petInfo: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .fill(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 84)
+                .cornerRadius(15)
+            HStack(alignment: .center, spacing: 5) {
+                VStack(alignment: .leading) {
+                    Text(viewModel.detail?.subCategory ?? "")
+                        .applySubtitle(color: .disabledTextColor)
+                    Text(viewModel.detail?.name ?? "")
+                        .applyBigTitle(color: .mainTextColor)
+                }
+                
+                Spacer()
+                
+                Text("\(viewModel.changeGenderToImage(viewModel.detail?.gender ?? "MALE"))")
+                    .padding(5)
+                    .background(Circle().fill(Color.homeSubFilter))
+                
+                Text(viewModel.changeMonthToAge(viewModel.detail?.age ?? 0))
+                    .applySubtitle(color: .mainTextColor)
+                    .padding(5)
+                    .background(RoundedRectangle(cornerRadius: 75).fill(Color.homeSubFilter))
+            }
+            .padding()
+        }
+        .padding([.leading, .trailing])
+        .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+    }
+    
+    private var petPrice: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .fill(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 84)
+                .cornerRadius(15)
+            HStack(alignment: .center, spacing: 5) {
+                Text("책임비")
+                    .applyBigTitle(color: .mainTextColor)
+                
+                Spacer()
+                
+                Text("\(viewModel.detail?.money ?? 0)원")
+                    .applyBigTitle(color: .mainTextColor)
+            }
+            .padding()
+        }
+        .padding([.leading, .trailing])
+        .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+    }
+    
+    private var healthInfo: some View {
+        VStack {
+            Text("기본정보")
+                .applySubhead(color: .mainTextColor)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            CustomLazyVGrid(col: 2, spacing: 10) {
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity, minHeight: 77)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("예방접종")
+                            .applySubtitle(color: .disabledTextColor)
+                        Text(viewModel.detail?.vaccinated ?? "-")
+                            .applyInner(color: .mainTextColor)
+                    }
+                    .padding()
+                }
+                
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity, minHeight: 77)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("건강검짐")
+                            .applySubtitle(color: .disabledTextColor)
+                        Text(viewModel.detail?.medicalChecked ?? "-")
+                            .applyInner(color: .mainTextColor)
+                    }
+                    .padding()
+                }
+                
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity, minHeight: 77)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("반려동물 등록인증")
+                            .applySubtitle(color: .disabledTextColor)
+                        Text(viewModel.detail?.registered ?? "-")
+                            .applyInner(color: .mainTextColor)
+                    }
+                    .padding()
+                }
+                
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity, minHeight: 77)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("중성화")
+                            .applySubtitle(color: .disabledTextColor)
+                        Text(viewModel.detail?.neutered ?? "-")
+                            .applyInner(color: .mainTextColor)
+                    }
+                    .padding()
+                }
+            }
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+        }
+        .padding()
+    }
+    
+    private var diseaseInfo: some View {
+        VStack(alignment: .leading) {
+            Text("반려동물이 가지고 있는 질병")
+                .applySubhead(color: .mainTextColor)
+                .bold()
+            HStack(spacing: 10) {
+                ForEach(viewModel.diseaseInfo, id: \.self) { info in
+                    Text(info)
+                        .applySubtitle(color: .mainTextColor)
+                        .padding(3)
+                        .background(RoundedRectangle(cornerRadius: 35).fill(Color.disabledText).opacity(0.18))
+                }
+                Text("해당없음")
+                    .applyInner(color: .mainTextColor)
+                    .bold()
+                    .isHidden(!viewModel.diseaseInfo.isEmpty)
+            }
+            
+            Divider()
+        }
+        .padding([.leading, .trailing])
+    }
+    
+    private var reason: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("분양이유")
+                .applySubhead(color: .mainTextColor)
+            
+            Text(viewModel.detail?.reason ?? "")
+                .bold()
+                .applyInner(color: .mainTextColor)
+            
+            Divider()
+        }
+        .padding([.leading, .trailing])
+    }
+    
+    private var advantage: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("반려동물 자랑")
+                .applySubhead(color: .mainTextColor)
+            
+            Text(viewModel.detail?.advantage ?? "")
+                .bold()
+                .applyInner(color: .mainTextColor)
+            
+            Divider()
+        }
+        .padding([.leading, .trailing])
+    }
+    
+    private var disadvantage: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("주의할 점")
+                .applySubhead(color: .mainTextColor)
+            
+            Text(viewModel.detail?.disadvantage ?? "")
+                .bold()
+                .applyInner(color: .mainTextColor)
+        }
+        .padding([.leading, .trailing])
     }
 }
