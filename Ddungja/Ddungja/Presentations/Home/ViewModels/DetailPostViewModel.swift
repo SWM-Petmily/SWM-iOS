78f796755a782dffca2d96e2aacfe54f46d872fa
ohsg0226 <ohsg0226@naver.com>
2023-08-04 14:54:49 +0900

    [refactor] :  subject 이름 변경

--- Ddungja/Ddungja/Presentations/Home/ViewModels/DetailPostViewModel.swift
+++ Ddungja/Ddungja/Presentations/Home/ViewModels/DetailPostViewModel.swift
@@ -20,18 +20,47 @@ final class DetailPostViewModel: ObservableObject {
     @Published var buttonText = ""
     @Published var buttonDisabled = false
     
-    private var fetchMoreActionSubject = PassthroughSubject<(isWriter: Bool, isApply: Bool, state: String), Never>()
+    private var buttonActionSubject = PassthroughSubject<(isWriter: Bool, isApply: Bool, state: String), Never>()
     
     init(coordinator: CoordinatorProtocol, homeUsecase: HomeUsecaseInterface) {
         self.coordinator = coordinator
         self.homeUsecase = homeUsecase
         
-        fetchMoreActionSubject.sink { [weak self] (isWriter, isApply, state) in
+        buttonActionSubject.sink { [weak self] (isWriter, isApply, state) in
             guard let self = self else { return }
             self.buttonText = changeButtonText(isWriter, isApply, state)
             self.buttonDisabled = changeButtonAbled(isWriter, isApply, state)
         }
         .store(in: &cancellables)
+    }
+    
+    private func changeButtonAbled(_ isWriter: Bool, _ isApply: Bool, _ state: String) -> Bool {
+        if state == "SAVE" {
+            if isWriter {
+                return false
+            } else {
+                return isApply ? true : false
+            }
+        }
+        return false
+    }
+    
+    private func changeButtonText(_ isWriter: Bool, _ isApply: Bool, _ state: String) -> String {
+        if state == "COMPLETE" {
+            return "입양 완료된 게시물"
+        } else if state == "DELETE" {
+            return "삭제된 게시물"
+        } else {
+            if isWriter {
+                return "입양 완료"
+            } else {
+                if isApply {
+                    return "입양 신청 완료"
+                } else {
+                    return "입양 신청하기"
+                }
+            }
+        }
     }
     
     func getDetailPost(_ id: Int) {
@@ -43,7 +72,7 @@ final class DetailPostViewModel: ObservableObject {
                 self?.detail = vo
                 self?.imagesURLString = vo.images.map { (id: UUID().uuidString, url: $0.url) }
                 self?.diseaseInfo = vo.diseases.map { $0 }
-                self?.fetchMoreActionSubject.send((vo.isWriter, vo.isApply, vo.status))
+                self?.buttonActionSubject.send((vo.isWriter, vo.isApply, vo.status))
             }
             .store(in: &cancellables)
     }
@@ -64,35 +93,6 @@ final class DetailPostViewModel: ObservableObject {
     
     func changeGenderToImage(_ type: String) -> Image {
         return type == "MALE" ? Image("Male") : Image("Female")
-    }
-    
-    private func changeButtonAbled(_ isWriter: Bool, _ isApply: Bool, _ state: String) -> Bool {
-        if state == "SAVE" {
-            if isWriter {
-                return false
-            } else {
-                return isApply ? true : false
-            }
-        }
-        return false
-    }
-    
-    private func changeButtonText(_ isWriter: Bool, _ isApply: Bool, _ state: String) -> String {
-        if state == "COMPLETE" {
-            return "입양 완료된 게시물" //disabled
-        } else if state == "DELETE" {
-            return "삭제된 게시물" //disabled
-        } else {
-            if isWriter {
-                return "입양 완료"
-            } else {
-                if isApply {
-                    return "입양 신청 완료" //disabled
-                } else {
-                    return "입양 신청하기"
-                }
-            }
-        }
     }
 
     func push(_ id: Int) {
