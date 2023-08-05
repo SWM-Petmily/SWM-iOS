b070ed8b2ea168c6257d3372c917b96b6a3381fb
ohsg0226 <ohsg0226@naver.com>
2023-08-05 17:37:14 +0900

    [feat] : swipe해서 화면 뒤로가능 기능 추가

---
+++ Ddungja/Ddungja/Presentations/Common/PopGesture.swift
@@ -0,0 +1,20 @@
+//
+//  PopGesture.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/08/05.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+
