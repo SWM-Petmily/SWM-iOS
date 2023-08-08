4266dcdd87085cebf1125e6227fc2534af503822
ohsg0226 <ohsg0226@naver.com>
2023-08-08 22:05:30 +0900

    [feat] : 추가인증 초기 화면 구성 및 관련 이미지 Asset 추가

---
+++ Ddungja/Ddungja/Presentations/Register/Views/PetCertificationScene.swift
@@ -0,0 +1,128 @@
+//
+//  PetCertificationScene.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/08/08.
+//
+
+import SwiftUI
+
+struct PetCertificationScene: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("반려동물 분양등록을 완료했습니다!!")
+                    .bold()
+                    .applyTitle(color: .mainTextColor)
+                
+                Text("안전한 분양을 위해 반려동물들에 대한 추가 정보를 입력해주세요.")
+                    .applySubhead(color: .mainTextColor)
+                
+                Text("추가정보를 작성하면 더 높은 수준의 인증 뱃지를 받으며, 검색 노출도를 확보할 수 있습니다.")
+                    .applySubhead(color: .disabledTextColor)
+            }
+            
+            ZStack(alignment: .leading) {
+                ZStack(alignment: .trailing) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                    Image("Approved")
+                }
+                
+                HStack {
+                    Image("PetRegistration")
+                        .scaledToFill()
+                        .clipped()
+                        .padding(.leading)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("반려동물 등록 인증")
+                            .bold()
+                            .applyBigTitle(color: .mainTextColor)
+                        
+                        Text("동물 등록 번호를 이용한 반려동물을 인증하는 과정입니다.")
+                            .applySubhead(color: .mainTextColor)
+                    }
+                }
+                
+            }
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+            
+            ZStack(alignment: .leading) {
+                ZStack(alignment: .trailing) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                    Image("Rejected")
+                }
+                
+                
+                HStack {
+                    Image("Vaccination")
+                        .scaledToFill()
+                        .clipped()
+                        .padding(.leading)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("예방접종 인증")
+                            .bold()
+                            .applyBigTitle(color: .mainTextColor)
+                        
+                        Text("예방접종을 받은 반려동물을 인증하는 과정입니다.")
+                            .applySubhead(color: .mainTextColor)
+                    }
+                }
+                
+            }
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+            
+            ZStack(alignment: .leading) {
+                ZStack(alignment: .trailing) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                    Image("Waiting")
+                }
+                HStack {
+                    Image("HealthScreening")
+                        .scaledToFill()
+                        .clipped()
+                        .padding(.leading)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("건강검진정보 인증")
+                            .bold()
+                            .applyBigTitle(color: .mainTextColor)
+                        
+                        Text("건강검진 정보 업로드를 통해 반려동물의 질병과 상태를 파악하는 과정입니다.")
+                            .applySubhead(color: .mainTextColor)
+                    }
+                }
+                
+            }
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 0)
+            
+            
+            Button {
+                //                viewModel.push(.adoptionReason)
+            } label: {
+                Text("등록 1/6")
+                    .applyInner(color: .white)
+                    .frame(height: 52)
+                    .frame(maxWidth: .infinity)
+            }
+            .background(Color.main)
+            .cornerRadius(14)
+            .padding([.leading, .trailing, .bottom])
+        }
+        .padding()
+    }
+}
+
+struct PetCertificationScene_Previews: PreviewProvider {
+    static var previews: some View {
+        PetCertificationScene()
+    }
+}
