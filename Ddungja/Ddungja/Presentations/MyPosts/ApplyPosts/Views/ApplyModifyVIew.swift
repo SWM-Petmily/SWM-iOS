d68901842ff16e78416063c7799ed6a5f25b5c71
ohsg0226 <ohsg0226@naver.com>
2023-08-21 16:33:18 +0900

    [refactor] : 지원하기 화면 CustomAlert -> Alert 변경

--- Ddungja/Ddungja/Presentations/MyPosts/ApplyPosts/Views/ApplyModifyVIew.swift
+++ Ddungja/Ddungja/Presentations/MyPosts/ApplyPosts/Views/ApplyModifyVIew.swift
@@ -17,49 +17,52 @@ struct ApplyModifyVIew: View {
     }
     
     var body: some View {
-        ZStack {
-            CustomAlert(presentAlert: $viewModel.showAlert, alertType: .error(title: viewModel.errorTitle, message: viewModel.errorDetailMessage, icon: viewModel.errorIcon, iconColor: viewModel.errorIconColor), coordinator: viewModel.coordinator)
-                .isHidden(!viewModel.showAlert)
-            VStack {
-                ApplyCommonView(viewModel: viewModel.applyViewModel, postId: id)
-                    .navigationTitle("지원하기")
-                    .navigationBarBackButtonHidden()
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Image(systemName: "chevron.backward")
-                                .onTapGesture {
-                                    viewModel.pop()
-                                }
-                        }
-                    }
-                
-                CustomLazyVGrid(col: 2, spacing: 10) {
-                    Button {
-                        throttleButton(id: id)
-                    } label: {
-                        Text("삭제")
-                            .applyInner(color: .mainColor)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 45)
-                            .background(Color.rejectButton)
-                            .cornerRadius(15)
-                    }
-                    
-                    Button {
-                        viewModel.push(.editProfile(isRegister: true))
-                    } label: {
-                        Text("수정하기")
-                            .applyInner(color: .white)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 45)
-                            .background(Color.main)
-                            .cornerRadius(15)
+        VStack {
+            ApplyCommonView(viewModel: viewModel.applyViewModel, postId: id)
+                .navigationTitle("지원하기")
+                .navigationBarBackButtonHidden()
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Image(systemName: "chevron.backward")
+                            .onTapGesture {
+                                viewModel.pop()
+                            }
                     }
                 }
-                .padding([.leading, .trailing, .bottom])
+            
+            CustomLazyVGrid(col: 2, spacing: 10) {
+                Button {
+                    throttleButton(id: id)
+                } label: {
+                    Text("삭제")
+                        .applyInner(color: .mainColor)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 45)
+                        .background(Color.rejectButton)
+                        .cornerRadius(15)
+                }
+                
+                Button {
+                    viewModel.push(.editProfile(isRegister: true))
+                } label: {
+                    Text("수정하기")
+                        .applyInner(color: .white)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 45)
+                        .background(Color.main)
+                        .cornerRadius(15)
+                }
             }
+            .padding([.leading, .trailing, .bottom])
+        }
+        .alert(viewModel.errorTitle, isPresented: $viewModel.showAlert) {
+            Button("확인", role: .cancel) {
+                viewModel.pop()
+            }
+        } message: {
+            Text(viewModel.errorDetailMessage)
         }
     }
 }
