dabc9f3f349f39beecd65d9d05ca77b24210a467
ohsg0226 <ohsg0226@naver.com>
2023-09-05 23:26:08 +0900

    [feat] : WebView 추가

---
+++ Ddungja/Ddungja/Presentations/Common/WebView.swift
@@ -0,0 +1,23 @@
+//
+//  WebView.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/09/05.
+//
+
+import SwiftUI
+
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
