a25524714f98b6cf70b5bb1aa951edd88983f5c3
ohsg0226 <ohsg0226@naver.com>
2023-09-06 15:52:26 +0900

    [refactor] : webview 매개변수 타입 URL에서 string으로 변경

--- Ddungja/Ddungja/Presentations/Common/WebView.swift
+++ Ddungja/Ddungja/Presentations/Common/WebView.swift
@@ -10,14 +10,19 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let url: URL
+    let url: String
     
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        guard let url = URL(string: url) else {
+            return WKWebView()
+        }
+        let webView = WKWebView()
+        
+        webView.load(URLRequest(url: url))
+        return webView
     }
     
     func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
+
     }
 }
