3ac3a2354c8435ba3458a9e34b6b3fa2612eb1b1
ohsg0226 <ohsg0226@naver.com>
2023-09-12 22:15:29 +0900

    [feat] : UserProfileViewModel 테스트를 위한 MockProfileUsecaseInterface 코드 추가

---
+++ Ddungja/DdungjaTests/MockProfileUsecaseInterface.swift
@@ -0,0 +1,52 @@
+//
+//  MockProfileUsecaseInterface.swift
+//  DdungjaTests
+//
+//  Created by 오승기 on 2023/09/12.
+//
+
+import Combine
+@testable import Ddungja
+
+final class MockProfileUsecaseInterface: ProfileUsecaseInterface {
+    var getMyPageReturnValue: Result<MyPageVO, CustomErrorVO>?
+    var getUserProfileReturnValue: Result<ProfileVO, CustomErrorVO>?
+    var putEditUserProfileReturnValue: Result<Int, CustomErrorVO>?
+    var postEditProfileReturnValue: Result<Int, CustomErrorVO>?
+
+    func getMyPage() -> AnyPublisher<MyPageVO, CustomErrorVO> {
+        if let returnValue = getMyPageReturnValue {
+            return Result.Publisher(returnValue)
+                .eraseToAnyPublisher()
+        } else {
+            fatalError("getMyPageReturnValue not set in the mock")
+        }
+    }
+
+    func getUserProfile() -> AnyPublisher<ProfileVO, CustomErrorVO> {
+        if let returnValue = getUserProfileReturnValue {
+            return Result.Publisher(returnValue)
+                .eraseToAnyPublisher()
+        } else {
+            fatalError("getUserProfileReturnValue not set in the mock")
+        }
+    }
+
+    func putEditUserProfile(profile: ProfileEditVO) -> AnyPublisher<Int, CustomErrorVO> {
+        if let returnValue = putEditUserProfileReturnValue {
+            return Result.Publisher(returnValue)
+                .eraseToAnyPublisher()
+        } else {
+            fatalError("putEditUserProfileReturnValue not set in the mock")
+        }
+    }
+
+    func postEditProfile(profile: ProfileEditVO) -> AnyPublisher<Int, CustomErrorVO> {
+        if let returnValue = postEditProfileReturnValue {
+            return Result.Publisher(returnValue)
+                .eraseToAnyPublisher()
+        } else {
+            fatalError("postEditProfileReturnValue not set in the mock")
+        }
+    }
+}
