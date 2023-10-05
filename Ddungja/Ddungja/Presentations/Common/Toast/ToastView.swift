778511484e56e24f10c838a2ff088c2e08254fd8
ohsg0226 <ohsg0226@naver.com>
2023-10-05 20:52:39 +0900

    [feat] : Toast 코드 추가

---
+++ Ddungja/Ddungja/Presentations/Common/Toast/ToastView.swift
@@ -0,0 +1,61 @@
+//
+//  ToastView.swift
+//  Ddungja
+//
+//  Created by 오승기 on 2023/10/05.
+//
+
+import SwiftUI
+
+struct Toast: Equatable {
+    var type: ToastStyle
+    var title: String
+    var message: String
+    var duration: Double = 3
+}
+
+struct ToastView: View {
+    var type: ToastStyle
+    var title: String
+    var message: String
+    var onCancelTapped: (() -> Void)
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Image(systemName: type.iconFileName)
+                    .foregroundColor(type.themeColor)
+                
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text(message)
+                        .font(.system(size: 12))
+                        .foregroundColor(Color.black.opacity(0.6))
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button {
+                    onCancelTapped()
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color.black)
+                }
+            }
+            .padding()
+        }
+        .background(Color.white)
+        .overlay(
+            Rectangle()
+                .fill(type.themeColor)
+                .frame(width: 6)
+                .clipped()
+            , alignment: .leading
+        )
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .cornerRadius(8)
+        .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: 1)
+        .padding(.horizontal, 16)
+    }
+}
